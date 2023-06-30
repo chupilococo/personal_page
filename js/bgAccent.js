cb80a57bc14ec91546d715dc417fd227dc2269f0
danilo carena <44451507+chupilococo@users.noreply.github.com>
2023-06-30 12:20:43 -0300

    style

--- js/bgAccent.js
+++ js/bgAccent.js
@@ -1,6 +1,9 @@
-const blobs = document.querySelectorAll(".blob");
+const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-const animateBlob = (x, y) => {
+const animateBlob = (x, y, blobs) => {
+    // console.log(x, y);
     blobs.forEach((blob, index) => {
         blob.animate(
             {
@@ -15,11 +18,27 @@ const animateBlob = (x, y) => {
 }
 
 
-document.body.onpointermove = (e) => {
-    const { clientX, clientY } = e;
-    animateBlob(clientX, clientY);
-};
-
-if ((window.innerWidth || document.documentElement.clientWidth) > 850) {
-    // do something
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const bgAccent = document.querySelector('#bg_accent');
+    for (let index = 0; index < 5; index++) {
+        const blob = document.createElement('div');
+        blob.classList.add('blob')
+        bgAccent.appendChild(blob);
+    }
+    const blobs = document.querySelectorAll(".blob");
+    if ((window.innerWidth || document.documentElement.clientWidth) > 850) {
+        document.body.onpointermove = (e) => {
+            const { clientX, clientY } = e;
+            animateBlob(clientX, clientY, blobs);
+        };
+    } else {
+        setInterval(
+            () => {
+                animateBlob(
+                    randomIntFromInterval(0, parseInt(document.documentElement.clientWidth)),
+                    randomIntFromInterval(0, parseInt(document.documentElement.clientHeight))
+                )
+            },
+            5000)
+    }
+})
